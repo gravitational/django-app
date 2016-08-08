fbe837f39bf6701af6eab2c76ce405d567da64ed
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-08-08 19:11:25 +0700

    add django entrypoint

--- images/django/entrypoint.sh
+++ images/django/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Collect static files
 echo "Collect static files"
 python3 manage.py collectstatic --noinput
 
-# Apply database migrations
 echo "Apply database migrations"
 python3 manage.py migrate
 
-# Start server
 echo "Starting server"
 python3 manage.py runserver 0.0.0.0:8000
